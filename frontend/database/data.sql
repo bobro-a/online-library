d21a18e4cc5b525d84faab2687de496738c03df7
bobro-a <m3_ba@mail.ru>
2025-05-07 15:34:19 +0300

    добавила новые поля

--- frontend/database/data.sql
+++ frontend/database/data.sql
@@ -10,10 +10,12 @@ create table books(
     book_path text not null,
     cover_path text not null,
     author text not null,
-    tags text not null
+    tags text not null,
+    rating numeric,
+    year int
 );
-insert into books(title,book_path,cover_path,author,tags)
+insert into books(title,book_path,cover_path,author,tags,rating,year)
 values
-    ('Анна Каренина','/database/books/Anna_Karenina.pdf','/database/covers/Anna_Karenina.jpg','Лев Николаевич Толстой','классика'),
-    ('Мастер и Маргарита','/database/books/Master_Margarita.pdf','/database/covers/Master_Margarita.jpg','Михаил Афанасьевич Булгаков','классика'),
-    ('Портрет Дориана Грея','/database/books/Portret_Doriana_Greya.pdf','/database/covers/Portret_Doriana_Greya.jpg','Оскар Уайлд','зарубежная классика');
+    ('Анна Каренина','/database/books/Anna_Karenina.pdf','/database/covers/Anna_Karenina.jpg','Лев Николаевич Толстой','классика',0,1878),
+    ('Мастер и Маргарита','/database/books/Master_Margarita.pdf','/database/covers/Master_Margarita.jpg','Михаил Афанасьевич Булгаков','классика',0,1967),
+    ('Портрет Дориана Грея','/database/books/Portret_Doriana_Greya.pdf','/database/covers/Portret_Doriana_Greya.jpg','Оскар Уайлд','зарубежная классика',0,1890);

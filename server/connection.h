0f072cd3c28b9ea88251d8b843c2d43ccd05c742
bobro-a <m3_ba@mail.ru>
2025-05-07 15:39:09 +0300

    удалила connection

--- server/connection.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-pqxx::work conn() {
-    pqxx::connection conn("dbname=online-library user=postgres password=secret host=localhost");
-    pqxx::work txn(conn);
-    return txn;
-}

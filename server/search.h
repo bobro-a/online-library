765bc5dc117a3c0f5f0dde4d7894023638a838da
bobro-a <m3_ba@mail.ru>
2025-05-15 12:09:46 +0300

    реализовала поиск книги по параметру

--- server/search.h
+++ server/search.h
@@ -1,20 +1,40 @@
-#pragme once
+#pragma once
+#include <netinet/in.h>
+#include <unistd.h>
 #include <optional>
 #include <vector>
+#include <pqxx/pqxx>
+#include <iostream>
+#include "book.h"
+
 using namespace std;
 
-extern pqxx::connection* conn;
 
-vector<Book> searchBooksBy(vector<Book> &books, string& parametr,typename value) {
+extern pqxx::connection *conn;
+
+vector<Book> searchBooksBy(const string &parametr, const string& value) {
     vector<Book> result;
     try {
         pqxx::work txn(*conn);
-        string query="SELECT * FROM books";
-        if (!parametr.empty()) {
-            query+=" WHERE ";
-            if (parametr=="title") query+="title ILIKE %"+value+"%";
+        string query = "SELECT * FROM books";
+        if (!parametr.empty() && !value.empty()) {
+            query += " WHERE ";
+            if (parametr=="rating" || parametr=="year") query += parametr + " = " + value;
+            else query += parametr + " ILIKE '%" + value + "%'";
         }
-        auto r = txn.exec_params(query);
+        auto r = txn.exec(query);
+        for (auto row : r) {
+            Book book;
+            book.id = row["id"].as<int>();
+            book.title = row["title"].as<string>();
+            book.author = row["author"].as<string>();
+            book.cover_url = row["cover_path"].as<string>();
+            book.pdf_url = row["book_path"].as<string>();
+            book.tags = row["tags"].as<string>();
+            book.rating = row["rating"].as<double>();
+            book.year = row["year"].as<int>();
+            result.push_back(book);
+        }
     } catch (const exception &e) {
         cerr << "DB Error: " << e.what() << endl;
     }

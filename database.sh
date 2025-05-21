9d27ab8839075c4a1c94f4be0d2f1e27a1205907
bobro-a <m3_ba@mail.ru>
2025-05-21 13:51:52 +0300

    теперь база данных создаётся через bash файл

--- database.sh
+++ database.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-set -e
-
-# Переход под пользователем postgres
-sudo -i -u postgres bash <<EOF
-
-# Удаление базы (если есть)
-psql -c "DROP DATABASE IF EXISTS \\"online-library\\";"
-
-# Создание базы
-psql -c "CREATE DATABASE \\"online-library\\";"
-
-# Импорт таблиц и данных
-psql -d "online-library" -f "/rontend/database/data.sql"
-
-EOF
-
+sudo -u postgres psql -c "drop database if exists \"online-library\";"
+sudo -u postgres psql -c "CREATE DATABASE \"online-library\"";
+sudo -u postgres psql -d "online-library" -f frontend/database/data.sql
 echo "✅ База данных online-library создана и заполнена."
